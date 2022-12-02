7657b9fe8f2ebdc3924f917a4e14dde268d61edb
Alexander Kuzin <61850604+Por7ableWorlD@users.noreply.github.com>
2022-12-02 22:03:52 +0400

    Completed 14.4

--- Skillbox 13.4/Helpers.h
+++
@@ -1,4 +0,0 @@
-int SumSquare(int FirstNumber, int SecondNumber)
-{
-	return ((FirstNumber + SecondNumber) * (FirstNumber + SecondNumber));
-}
---
+++ Skillbox/Skillbox.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string line;
+
+	std::cout << "Enter ur string: ";
+	getline(std::cin, line);
+
+	std::cout << "\n\nUr string: " << line << "\nLength = " << line.length() <<
+		"\n\nFirst symbol: " << line[0] << "\nSecond symbol: " << line[line.length() - 1] << std::endl;
+
+
+	return 0;
+}
