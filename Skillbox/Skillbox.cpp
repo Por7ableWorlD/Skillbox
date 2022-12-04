6f2d02a673801713b696ff19779cee59f05cfccb
Alexander Kuzin <61850604+Por7ableWorlD@users.noreply.github.com>
2022-12-04 14:33:23 +0400

    Completed 16.5

--- Skillbox/Skillbox.cpp
+++ Skillbox/Skillbox.cpp
@@ -1,61 +1,38 @@
 ﻿#include <iostream>
-
-//#define FirstTask
-//#define FirstTaskBrief
-
-void OutputEvenNumbersToLimit(int Limit, bool IsEven);
+#include <ctime>
 
 int main()
 {
-	const int N = 10;
+	const int N = 5;
+	int Arr[N][N]{};
 
-#ifdef FirstTask
-	std::cout << "Even numbers from 0 to " << N << std::endl;
-	for (int i = 1; i < N; ++i)
+	time_t EpochStartSecondsAmount = time(NULL);
+
+	tm CurrentDateTime;
+	localtime_s(&CurrentDateTime, &EpochStartSecondsAmount);
+
+	int SumLineElements = 0;
+	int Line = CurrentDateTime.tm_mday % 5;
+
+	std::cout << "Array:\n";
+	for (int i = 0; i < N; i++)
 	{
-		if ((i % 2) == 0)
+		for (int j = 0; j < N; j++)
 		{
-			std::cout << i << " ";
+			Arr[i][j] = i + j;
+
+			if (i == Line)
+			{
+				SumLineElements += Arr[i][j];
+			}
+
+			std::cout << Arr[i][j] << " ";
 		}
+
+		std::cout << std::endl;
 	}
 
-	std::cout << std::endl;
-#endif // FirstTask
-
-#ifdef FirstTaskBrief
-	std::cout << "Even numbers from 0 to " << N << std::endl;
-	for (int i = 2; i < N; i+=2)
-	{
-		std::cout << i << " ";
-	}
-
-	std::cout << std::endl;
-#endif // FirstTaskBrief
-
-	std::cout << "Even numbers from 0 to " << N << std::endl;
-	OutputEvenNumbersToLimit(N, true);
-
-	std::cout << "\n\nOdd numbers from 0 to " << N << std::endl;
-	OutputEvenNumbersToLimit(N, false);
-	std::cout << std::endl;
+	std::cout << "\nSum elements of " << Line << "(index) row = " << SumLineElements << std::endl;
 
 	return 0;
-}
-
-void OutputEvenNumbersToLimit(int Limit, bool IsEven)
-{
-	if (IsEven)
-	{
-		for (int i = 2; i < Limit; i+=2)
-		{
-			std::cout << i << " ";
-		}
-	}
-	else
-	{
-		for (int i = 1; i < Limit; i+=2)
-		{
-			std::cout << i << " ";
-		}
-	}
 }
