a1b2e737fe5aaea6464988a046fd4eacac4b1aa6
Alexander Kuzin <61850604+Por7ableWorlD@users.noreply.github.com>
2022-12-06 13:25:58 +0400

    Completed 17.5

--- Skillbox/Skillbox.cpp
+++ Skillbox/Skillbox.cpp
@@ -1,38 +1,66 @@
 ﻿#include <iostream>
-#include <ctime>
+#include <cmath>
+
+class Vector
+{
+private:
+	double XAxis, YAxis, ZAxis;
+
+public:
+	Vector() : XAxis(0), YAxis(0), ZAxis(0) {}
+	Vector(double x, double y, double z) : XAxis(x), YAxis(y), ZAxis(z) {}
+
+	void Set(double x, double y, double z)
+	{
+		XAxis = x;
+		YAxis = y;
+		ZAxis - z;
+	}
+
+	Vector* GetCopy()
+	{
+		return this;
+	}
+
+	void Output()
+	{
+		std::cout << "Vector: (" << XAxis << ',' << YAxis << ',' << ZAxis << ')' << std::endl;
+	}
+
+	void OutputXAxis()
+	{
+		std::cout << "XAxis = " << XAxis << std::endl;
+	}
+
+	void OutputYAxis()
+	{
+		std::cout << "YAxis = " << YAxis << std::endl;
+	}
+
+	void OutputZAxis()
+	{
+		std::cout << "ZAxis = " << ZAxis << std::endl;
+	}
+
+	double CalculateLength()
+	{
+		return sqrt(XAxis * XAxis + YAxis * YAxis + ZAxis * ZAxis);
+	}
+
+};
 
 int main()
 {
-	const int N = 5;
-	int Arr[N][N]{};
+	Vector A(5, 2, 1);
+	Vector B;
+	Vector* ACopy = A.GetCopy();
 
-	time_t EpochStartSecondsAmount = time(NULL);
+	A.Output();
+	B.Output();
+	ACopy->Output();
 
-	tm CurrentDateTime;
-	localtime_s(&CurrentDateTime, &EpochStartSecondsAmount);
-
-	int SumLineElements = 0;
-	int Line = CurrentDateTime.tm_mday % 5;
-
-	std::cout << "Array:\n";
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			Arr[i][j] = i + j;
-
-			if (i == Line)
-			{
-				SumLineElements += Arr[i][j];
-			}
-
-			std::cout << Arr[i][j] << " ";
-		}
-
-		std::cout << std::endl;
-	}
-
-	std::cout << "\nSum elements of " << Line << "(index) row = " << SumLineElements << std::endl;
+	std::cout << "\nAVector length = " << A.CalculateLength() << std::endl;
+	std::cout << "BVector length = " << B.CalculateLength() << std::endl;
 
 	return 0;
 }
